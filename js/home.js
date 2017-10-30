67742ed95485571e2b3cb8b3ae0407f13ea2b73e
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2017-10-30 18:46:29 +0530

    Completed With Home

--- js/home.js
+++ js/home.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             });
         } else {
             $('#home .logoContainer').css({
-                position: 'relative'
+                position: 'absolute'
             });
         }
     });
