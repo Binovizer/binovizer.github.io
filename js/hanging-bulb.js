5ae1e06d355baf0556cca9e76e39efc81caa4e7c
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2018-11-01 11:47:12 +0530

    Hire me fix

--- js/hanging-bulb.js
+++ js/hanging-bulb.js
@@ -4,12 +4,18 @@ $(document).ready(function(){
         if ( $(".bulb-container .rope .bulb").hasClass("on") ) {
             $("#landing #character").css("opacity", "0");
             $("#landing #chatBox").css("opacity", "0");
-            $("#landing #back-to-work").css("opacity", "1");
+            $("#landing #back-to-work").css({
+                "opacity": "1",
+                "z-index": "999999"
+            });
 
         }else{
             $("#landing #character").css("opacity", "1");
             $("#landing #chatBox").css("opacity", "1");
-            $("#landing #back-to-work").css("opacity", "0");
+            $("#landing #back-to-work").css({
+                "opacity": "1",
+                "z-index": "-999999"
+            });
         }
     });
 });
